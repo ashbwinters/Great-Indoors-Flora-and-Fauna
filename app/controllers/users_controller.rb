ca9f40fb58f3a4fb75ababfab298e7f90f9584b9
github username <github email address>
2020-05-26 11:32:59 -0600

    working password encryption

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def create
         @user = User.create(
             username: params[:username],
-            password: params[password]
+            password: params[:password]
         )
         render json: {user: @user}, status: :created
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_secure-password
+    has_secure_password
 
 end
